da146a85ccfe8ade5cb41793cfe5d57d69082746
tadeoser2005-web <tadeoser2005@gmail.com>
2026-09-18 17:01:28 -0600

    creacion archivo cliente.js

---
+++ Clientes.js
@@ -0,0 +1,55 @@
+const prompt = require('prompt-sync')();
+
+let productos = ["Hamburguesa"];
+
+let pedidos = ["agua", "hot dog"];
+
+const carrito = [];
+
+let respuesta = "";
+
+function consultarProductos() {
+
+        console.log("Consultando producto....");
+        console.log(productos);
+
+}
+
+function crearPedidos() {
+
+    
+        while(true) {
+
+         // respuesta = prompt("Ingresa un producto o escribe salir para terminar:");
+            respuesta = prompt("Ingresa un producto o escribe salir para terminar:");
+
+            if(respuesta == null || respuesta.toLowerCase() == 'salir') {
+                    break;
+
+            }
+
+            if(respuesta.trim() != "") {
+
+                carrito.push(respuesta);
+                console.log(`${respuesta} agregado al carrito`);
+
+            }
+
+        }
+        console.log("");
+        console.log("Contenido actual del carrito");
+        console.log(carrito);
+        console.log("")
+
+}
+
+function listarPedidos() {
+
+        console.log("Imprimiendo pedidos.......");
+        console.log(pedidos);
+
+}
+
+consultarProductos()
+console.log(crearPedidos())
+listarPedidos()
---
+++ Investigacion.md
@@ -0,0 +1,24 @@
+************************   CONSOLE LOG ***************************************
+
+            Permite imprimir info en la terminal, por defecto muestra texto plano 
+
+************************* Funciones **********************************************
+
+                Bloques de codigo reutilizables disenado spara realizar tareas especificas
+                mostrarmenu()
+                consultarProducto()
+
+
+************************** Template String ********************************************
+
+                Cadena liteerales, herramienta poderosa para formatear el menu, se escribe utilizando comillas invertidas  " ` "
+
+                Tienen dos super funciones 
+
+                1. Multilinea: Permite dar enter y escribir texto en varias lineas sin necesidad de concatenar cdenas o poner salto de linea en cada renglon, lo que dibujes en los backticks aparecera tal cual en la consola
+
+                2. Interpolacion: Puedes inyectar variables dinamicas directamente dentro del texto usando la sintaxis ${variable}
+
+
+
+                function consultarProductos 
